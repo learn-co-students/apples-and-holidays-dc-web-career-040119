fd2424edd39ca0d9e3822dd17749521eb8a081db
mario rodrigueza <mariorodriguezan@gmail.com>
2019-03-27 06:59:24 +0000

    Done.

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -18,30 +18,35 @@ def second_supply_for_fourth_of_july(holiday_hash)
   #   }
   # }
   # return the second element in the 4th of July array
+  holiday_hash[:summer][:fourth_of_july][1]
 end
 
 def add_supply_to_winter_holidays(holiday_hash, supply)
   # holiday_hash is identical to the one above
   # add the second argument, which is a supply, to BOTH the
   # Christmas AND the New Year's arrays
-
+  holiday_hash[:winter][:christmas] << supply
+  holiday_hash[:winter][:new_years] << supply
 end
 
 
 def add_supply_to_memorial_day(holiday_hash, supply)
   # again, holiday_hash is the same as the ones above
   # add the second argument to the memorial day array
+  holiday_hash[:spring][:memorial_day] << supply
 
 end
 
 def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_array)
   # code here
   # remember to return the updated hash
-
+  holiday_hash[season][holiday_name] = supply_array
+return holiday_hash
 end
 
 def all_winter_holiday_supplies(holiday_hash)
   # return an array of all of the supplies that are used in the winter season
+  holiday_hash[:winter].values.flatten
 
 end
 
@@ -53,13 +58,28 @@ def all_supplies_in_holidays(holiday_hash)
   # Summer:
   #   Fourth Of July: Fireworks, BBQ
   # etc.
-
+   holiday_hash.collect do |season, values|
+        puts "#{season.capitalize}:"
+      values.collect do |holiday, supply|
+        newH = holiday.to_s.split("_")
+        puts "  #{newH.map{|x| x.capitalize}.join(" ")}: #{supply.join(", ")}"
+        end
+      end
 end
-
 def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
-
+holiday_names = []
+    holiday_hash.collect do |season , holiday|
+      holiday.collect do |values , supply|
+        supply.collect do |items|
+        if items == "BBQ"
+          holiday_names << values
+          end
+        end
+      end
+    end
+    holiday_names
 end
 
 
