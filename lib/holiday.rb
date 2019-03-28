0746f1a3e7170df093480b9a030a50fd7f415167
Nosa Okundaye <okundaye.preciousnosa@gmail.com>
2019-03-28 07:59:38 +0000

    Done.

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -18,6 +18,15 @@ def second_supply_for_fourth_of_july(holiday_hash)
   #   }
   # }
   # return the second element in the 4th of July array
+  holiday_hash.each do |key, value|
+    if key == :summer
+          value.each do |attribute, data|
+            data.each do |val|
+                return val[-1]
+          end
+        end
+      end
+    end
 end
 
 def add_supply_to_winter_holidays(holiday_hash, supply)
